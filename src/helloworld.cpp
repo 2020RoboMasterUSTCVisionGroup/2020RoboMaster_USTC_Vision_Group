bbeb30da4207c2df25629ec7ff573f55378c3895
supersyz <362871010@qq.com>
2019-11-02 21:37:48 +0800

    syzsyz

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,9 +1,11 @@
 #include  <stdio.h>
-
+#include <iostream>
 int main()
 {
     printf("hellwhhhorld\n");
+    cout<<"please use cout"
     printf("not hahahahhello\n");
+    cout<<"lalala"
 
     int a =1;
     return 0;
