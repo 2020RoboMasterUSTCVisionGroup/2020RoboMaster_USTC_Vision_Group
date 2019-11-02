9bbed8b00f7f98d22387a0cbcc1fac2f0bdb52f2
AladingMagi <pujiang_u@163.com>
2019-11-03 02:38:03 +0800

    test

--- src/display.cpp
+++ src/display.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[], char **env)
 	cameraTransfer();
 	int nRet = -1;
 	void* m_handle = NULL;
- 	printf("helloworld!\n");
+ 	printf("helloworld!11111\n");
 	//枚举子网内指定的传输协议对应的所有设备
 	unsigned int nTLayerType = MV_USB_DEVICE;
 	MV_CC_DEVICE_INFO_LIST m_stDevList = { 0 };
