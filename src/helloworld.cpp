b28a179818a1f2b441c96e3785f6bc9c7c2dd706
hoaxyee <hoaxy88@163.com>
2019-11-06 06:46:31 -0800

    first commit

---
+++ src/helloworld.cpp
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+#include <iostream>
+int main()
+{
+    printf("hellwhhhorld\n");
+    cout<<"please use cout"
+    printf("not hahahahhello\n");
+    cout<<"lalala"
+    printf("20191103-zhuyj")
+    printf("USTC DAILANG");  //戴浪
+    printf("2020robomaster"); //zhangjingyi
+    printf("hoaxy88.");//hoaxy
+    int a =1;
+    return 0;
+}
