edc6dbbd725b3dbcc64e43cc706d619b37052e25
AladingMagi <pujiang_u@163.com>
2019-11-16 19:01:03 +0800

    add some info

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include "AmorFinder/amorFinder.h"
 #include "Log/log.h"
 int main(){
+    //time collect start
     TIMER_START(x);
+
+    //construct AmorFinder instance,try to find lightbolbs
     AmorFinder finder;
     finder.setImage("./test.png");
     finder.preProcess(200);
     LightBlobs light_Blobs = finder.findLightBlob();
     finder.showLightBlobs("result",light_Blobs);
+
+    //time collect finish,show the time
     TIMER_STOP(x);
     TIME_COST_MS("AmorFinder");
 }
