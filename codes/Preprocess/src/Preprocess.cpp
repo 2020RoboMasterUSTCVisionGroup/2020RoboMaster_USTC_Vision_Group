b7872406da1b927bfd40882e2f03ae5afb7e6f63
wyl2019 <wangyanlu2008@163.com>
2020-02-29 17:38:50 +0800

    浮动阈值 wyl

--- codes/Preprocess/src/Preprocess.cpp
+++ codes/Preprocess/src/Preprocess.cpp
@@ -7,11 +7,17 @@
 #include "Preprocess.h"
 
 char EnemyColor='r';
+int blockSize = 5;     //自适应阈值法的参数
+int constValue =10;
 void Preprocess::run(cv::Mat &src)
 {
     clearWhiteLight(src);
     cvtColor(src, src, COLOR_GRAY2RGB);     //将黑白图像转换成三通道
+    adaptiveThreshold(src,src, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY_INV, blockSize, constValue);//自适应阈值法二值化
+    uchar thresh=Otsu(src);
+    threshold(src,src,thresh,255,CV_THRESH_OTSU);
 }
+
 
 void Preprocess::clearWhiteLight(Mat &src)
 {
@@ -29,4 +35,60 @@ void Preprocess::clearWhiteLight(Mat &src)
         addWeighted(src_red, -1, src_blue,1, 0.0, src);     //蓝减红        
         
     }
+}
+//计算大津法的参数thresh
+ uchar Otsu(Mat &img)
+{
+	int i,j;
+	//行列
+	int row = img.rows;
+	int col = img.cols;
+	double rc = row*col;
+	//存储各级灰度的个数 初始为0
+	int n[256] = {0};
+	uchar *ptr;
+	for(i = 0; i < row; ++i)  
+    {  
+        ptr = img.ptr<uchar>(i);  
+        for (j = 0; j < col; ++j)  
+        {  
+            n[ptr[j]]++;
+        }  
+    }  
+	
+	//归一化 并且 计算累积和 、 累计均值 和 全局均值 
+	double p[256];   
+	double m[256];
+	double mg;
+ 
+	//计算概率的时候注意 如果都是整型 结果为0 
+	p[0] = n[0]/rc;
+	m[0] = 0;
+	mg = 0;
+ 
+    for(i = 1;i < 256; i++)
+	{
+		//计算概率 求全局均值
+		p[i] = n[i]/rc;
+		m[i] = i*p[i];
+		mg += m[i];
+        //进行累加
+        p[i]+=p[i-1];
+		m[i]+=m[i-1];
+	}
+ 
+	//阈值
+	uchar k = 0;
+	//类间方差 
+	double a = 0,a2;
+	for(i = 0;i < 256; i++)
+	{       //这里少加个判断 if(p[i]==0||p[i]==1) continue;
+		a2 = (mg*p[i]-m[i])*(mg*p[i]-m[i])/(p[i]*(1-p[i]));    
+		if(a2 > a)
+		{
+			k = i;
+			a = a2;
+		}
+	}
+	return k;
 }
